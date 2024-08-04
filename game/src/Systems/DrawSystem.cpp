b22b6c6227f03d9f8757f7d5b837dd1d016ed8f5
Francisco Javier Bahamondes Castro <francisco.bahamondes90@gmail.com>
2024-08-04 13:32:00 -0400

    ahora si

--- game/src/Systems/DrawSystem.cpp
+++ game/src/Systems/DrawSystem.cpp
@@ -13,57 +13,56 @@
 void DrawSystem::Update()
 {
     DoForEachComponent<TextureComponent>([this](TextureComponent& component)
+    {
+        // Draw spaceship
+        SpaceshipComponent* spaceship = ECSContainer.TryGetComponent<SpaceshipComponent>(component.EntityId);
+        if (spaceship)
         {
-            // Draw spaceship
-            SpaceshipComponent* spaceship = ECSContainer.TryGetComponent<SpaceshipComponent>(component.EntityId);
-            if (spaceship)
+            TransformComponent* transform = ECSContainer.TryGetComponent<TransformComponent>(component.EntityId);
+            DrawTextureV(component.Texture, transform->Position, WHITE);
+        }
+        // Draw aliens
+        AlienComponent* alien = ECSContainer.TryGetComponent<AlienComponent>(component.EntityId);
+        if (alien)
+        {
+            TransformComponent* transform = ECSContainer.TryGetComponent<TransformComponent>(component.EntityId);
+            DrawTextureV(component.Texture, transform->Position, WHITE);
+        }
+        // Draw mysteryship
+        MysteryshipComponent* mysteryship = ECSContainer.TryGetComponent<MysteryshipComponent>(component.EntityId);
+        if (mysteryship)
+        {
+            ActiveStateComponent* active = ECSContainer.TryGetComponent<ActiveStateComponent>(component.EntityId);
+            if (active)
             {
-                TransformComponent* transform = ECSContainer.TryGetComponent<TransformComponent>(component.EntityId);
-                DrawTextureV(component.Texture, transform->Position, WHITE);
-            }
-            // Draw aliens
-            AlienComponent* alien = ECSContainer.TryGetComponent<AlienComponent>(component.EntityId);
-            if (alien)
-            {
-                TransformComponent* transform = ECSContainer.TryGetComponent<TransformComponent>(component.EntityId);
-                DrawTextureV(component.Texture, transform->Position, WHITE);
-            }
-            // Draw mysteryship
-            MysteryshipComponent* mysteryship = ECSContainer.TryGetComponent<MysteryshipComponent>(component.EntityId);
-            if (mysteryship)
-            {
-                ActiveStateComponent* active = ECSContainer.TryGetComponent<ActiveStateComponent>(component.EntityId);
-                if (active)
+                if (active->Active)
                 {
-                    if (active->Active)
-                    {
-                        TransformComponent* transform = ECSContainer.TryGetComponent<TransformComponent>(component.EntityId);
-                        DrawTextureV(component.Texture, transform->Position, WHITE);
-                    }
+                    TransformComponent* transform = ECSContainer.TryGetComponent<TransformComponent>(component.EntityId);
+                    DrawTextureV(component.Texture, transform->Position, WHITE);
                 }
             }
+        }
+    });
 
-            // Draw lasers
-            LaserComponent* laser = ECSContainer.TryGetComponent<LaserComponent>(component.EntityId);
-            if (laser)
-            {
-                TransformComponent* transform = ECSContainer.TryGetComponent<TransformComponent>(component.EntityId);
-                if (transform)
-                {
-                    ActiveStateComponent* active = ECSContainer.TryGetComponent<ActiveStateComponent>(component.EntityId);
-                    if (active && active->Active)
-                    {
-                        DrawRectangleV(transform->Position, {4, 15}, {243, 216, 63, 255});
-                    }
-                }
-            }
-        });
-
-        // Draw blocks
-        DoForEachComponent<TransformComponent>([this](TransformComponent& component)
+    // Draw lasers
+    DoForEachComponent<TransformComponent>([this](TransformComponent& component)
+    {
+        LaserComponent* laser = ECSContainer.TryGetComponent<LaserComponent>(component.EntityId);
+        if (laser)
         {
-            BlockComponent* block = ECSContainer.TryGetComponent<BlockComponent>(component.EntityId);
-            if (block)
-                DrawRectangleV(component.Position, block->Size, block->Color);
-        });
+            ActiveStateComponent* active = ECSContainer.TryGetComponent<ActiveStateComponent>(component.EntityId);
+            if (active && active->Active)
+            {
+                DrawRectangleV(component.Position, {4, 15}, {243, 216, 63, 255});
+            }
+        }
+    });
+
+    // Draw blocks
+    DoForEachComponent<TransformComponent>([this](TransformComponent& component)
+    {
+        BlockComponent* block = ECSContainer.TryGetComponent<BlockComponent>(component.EntityId);
+        if (block)
+            DrawRectangleV(component.Position, block->Size, block->Color);
+    });
 }
